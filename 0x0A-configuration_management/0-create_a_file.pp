64bcb51b2f433fd3aa1088ceb37323c78c58f842
Abdulai <juniorrafiu@gmail.com>
2023-09-22 15:15:58 -0700

    hot

---
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -0,0 +1,8 @@
+#the code will create a file name school inside the /tmp directory
+file { '/tmp/school':
+  ensure   =>  file,
+  content  =>  'I love Puppet',
+  mode     =>  '07744',
+  owner    =>  'www-data',
+  group    =>  'www-data',
+}
