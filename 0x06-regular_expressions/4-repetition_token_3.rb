1b361fb9aec5522c82b68cec324104a7fab44234
juniorrafiu <juniorrafiu7@gmail.com>
2024-01-05 04:03:55 -0800

    hard

--- 0x06-regular_expressions/4-repetition_token_3.rb
+++ 0x06-regular_expressions/4-repetition_token_3.rb
@@ -1,2 +1,3 @@
 #!/usr/bin/env ruby
+#regular expression that will match a pattern
 puts ARGV[0].scan(/hbt*n/).join
--- 0x06-regular_expressions/5-beginning_and_end.rb
+++ 0x06-regular_expressions/5-beginning_and_end.rb
@@ -1,2 +1,3 @@
 #!/usr/bin/env ruby
+#regular expression that will match a pattern starting wth h ending in n
 puts ARGV[0].scan(/^h.n$/).join
